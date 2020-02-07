7e964a087bd106102a5854f105fdb221978a147e
zhao ziyi <zhaoziy@oregonstate.edu>
2020-02-07 09:23:48 -0800

    added some files and data

---
+++ data/alphabet.js
@@ -0,0 +1,25 @@
+const alphabet = {
+    details:[
+        { id:'a', letter:'А а', audio:'', description:'like 𝚊̲𝚛̲ in far' },
+        { id:'be', letter:'Б б', audio:'', description:'like 𝚋̲ in box' },
+        { id:'ve', letter:'В в', audio:'', description:'like 𝚟̲ in voie' },
+        { id:'ge', letter:'Г г', audio:'', description:'' },
+        { id:'de', letter:'А а', audio:'', description:'' },
+        { id:'ye', letter:'А а', audio:'', description:'' },
+        { id:'yo', letter:'А а', audio:'', description:'' },
+        { id:'zhe', letter:'А а', audio:'', description:'' },
+        { id:'ze', letter:'А а', audio:'', description:'' },
+        { id:'ee', letter:'А а', audio:'', description:'' },
+        { id:'ee_short', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+        { id:'a', letter:'А а', audio:'', description:'' },
+    ]
+
+
+};
